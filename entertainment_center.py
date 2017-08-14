757365ae369905d64fd336939bf35adca4dd0b8c
Sneha Kale <kalesneha88@gmail.com>
2017-08-14 14:27:55 -0400

    Changes after review 2
    Changes after review 2

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,22 +1,21 @@
 import media  # Importing media to use class Movie
 import fresh_tomatoes  # Importing fresh_tomatoes to open a movie website page
 import csv  # Importing csv module for csv file reading
-import os # Importing os module 
+import os  # Importing os module
 
 # creating a list of movies
 movies = []
 
-# Fetching records from csv file 
+# Fetching records from csv file
 csvfile = open(os.getcwd()+"\data.csv")
 data = csv.reader(csvfile, delimiter=',', quotechar='"')
 
-# Creating a list of movies with each record 
+# Creating a list of movies with each record
 for record in data:
-    movies.append(media.Movie(record[0],record[1],record[2],record[3]))
-    
+    movies.append(media.Movie(record[0], record[1], record[2], record[3]))
+
 # Closing the csv file after using it
 csvfile.close()
 
 # Calling a layout page
 fresh_tomatoes.open_movies_page(movies)
-        
--- media.py
+++ media.py
@@ -1,13 +1,15 @@
 import webbrowser  # Importing webbrowser module to use open method
 
+
 class Movie():
     """
         This is a class Movie.
-        It defines some features of the Movie eg. movie title, movie poster image etc.
+        It defines some features of the Movie
+        eg. movie title, movie poster image etc.
         Also has a method to show the trailer for a movie.
     """
-    
-    # Calling a construcor to initialize istance variables    
+
+    # Calling a construcor to initialize istance variables
     def __init__(self, movie_title, movie_storyline, movie_poster_image_url,
                  movie_trailer_youtube_url):
         self.title = movie_title
@@ -15,6 +17,6 @@ class Movie():
         self.poster_image_url = movie_poster_image_url
         self.trailer_youtube_url = movie_trailer_youtube_url
 
-    # A function to show trailer for a movie  
+    # A function to show trailer for a movie
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
